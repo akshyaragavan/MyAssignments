9345e30d945688bf31a6cdfade498f88ca38cf0e
akshyaragavan <akshyaragavan96@gmail.com>
2025-08-16 16:42:52 -0500

    Add files via upload
    HOME ASSIGNMENT 1

---
+++ EmployeeDetails.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str= "John Doe";
+		short age= 30;
+		float salary= 55000.75f;
+		char gender= 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber= 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber=7;
+		System.out.println(str+"\n"+age+"\n"+salary+"\n"+gender+"\n"+acceptTerms);
+		System.out.println(rating+"\n"+mobileNumber+"\n"+pinCode+"\n"+luckyNumber);
+		
+		
+
+	}
+
+}
---
+++ Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("the browser version is  "+ browserVersion+"\n");
+		         System.out.println("the browser name is  "+browserName+"\n");
+		         System.out.println("if it is visible  "+isVisible+"\n");
+		         System.out.println("the release year is  "+releaseYear+"\n");
+		         System.out.println("the browser logo is  "+browserLogo+"\n");
+		         
+				
+	}
+}
