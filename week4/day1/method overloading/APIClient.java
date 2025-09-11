fc591b08ef73df2f737874181fed6d194e7bca6c
akshyaragavan <akshyaragavan96@gmail.com>
2025-09-11 10:15:33 -0500

    Add files via upload

---
+++ week4/day1/method overloading/APIClient.java
@@ -0,0 +1,26 @@
+package week4.day1;
+
+public class APIClient {
+
+	public void sendRequest(String endpoint)
+	{
+		System.out.println(endpoint);
+	}
+	
+	public void sendRequest(String endpoint,String requestBody,boolean requestStatus )
+	{
+		System.out.println(endpoint);
+		System.out.println(requestBody);
+		System.out.println(requestStatus);
+	}
+	
+	public static void main(String[] args) {
+	
+		APIClient api=new APIClient();
+		api.sendRequest("a");
+		api.sendRequest("a", "b", true);
+		
+
+	}
+
+}
